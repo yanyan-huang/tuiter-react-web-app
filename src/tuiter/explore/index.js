7ad045698a740caa7a9968ba0729929a07fa9dc7
yanyan-huang <huang.yanya@northeastern.edu>
2023-02-28 17:20:02 -0800

    a6 React.js spring 2023

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -18,17 +18,24 @@ const ExploreComponent = () => {
             </div>
             <ul className="nav nav-pills mb-2">
                 <li className="nav-item">
-                    <a className="nav-link active">For You</a>
+                    <a href="#" className="nav-link active">For You</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">Trending</a>
+                    <a href="#" className="nav-link">Trending</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">News</a>
+                    <a href="#" className="nav-link">News</a>
                 </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Sports</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Entertainment</a>
+                </li>
+
             </ul>
             <div className="position-relative mb-2">
-                <img src="/images/spacex-starship.png" className="w-100"/>
+                <img src="/images/spacex-starship.png" alt="" className="w-100"/>
                 <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
